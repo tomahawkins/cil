1050ed3f0c0f682167b7b466d193a0f0fc3a936b
Tom <thomasdhawkins@eaton.com>
2010-05-11 05:42:14 +0000

    started parsing cil_types.mli

---
+++ CILTypes.hs
@@ -0,0 +1,19 @@
+module Main (main) where
+
+main :: IO ()
+main = do
+  f <- readFile "cil_types.mli"
+  putStrLn $ decomment f
+
+
+decomment :: String -> String
+decomment = decomment' 0
+  where
+  decomment' :: Int -> String -> String
+  decomment' n "" | n == 0    = ""
+                  | otherwise = error "reached end of file without closing comment"
+  decomment' n ('(':'*':a) = "  " ++ decomment' (n + 1) a
+  decomment' n ('*':')':a) | n > 0  = "  " ++ decomment' (n - 1) a
+                           | otherwise = error "unexpected closing comment"
+  decomment' n (a:b) = (if n > 0 && a /= '\n' then ' ' else a) : decomment' n b
+
--- Makefile
+++ Makefile
@@ -7,11 +7,15 @@ Language/CIL.hs: Language/CIL.DrIFT.hs
 test: Test.hs Language/CIL.hs
 	ghc --make -W -fglasgow-exts -o test Test.hs
 
+ciltypes: CILTypes.hs
+	ghc --make -W -fglasgow-exts -o ciltypes CILTypes.hs
+
 .PHONY: clean
 clean:
 	-rm *.o *.hi
 	-rm Language/*.o Language/*.hi
 	-rm test
+	-rm ciltypes
 
 .PHONY: clean-all
 clean-all: clean
